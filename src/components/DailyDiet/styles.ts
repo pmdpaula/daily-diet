a2746f33352caf92880fe8b5ffeae8b506684102
Pedro de Paula <pmdpaula@gmail.com>
2023-02-28 22:31:05 -0300

    feat(components): primeiros components

---
+++ src/components/DailyDiet/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  width: 100%;
+
+  /* margin-top: 24px; */
+  /* flex: 1; */
+  /* justify-content: center; */
+  /* align-items: center; */
+`;
